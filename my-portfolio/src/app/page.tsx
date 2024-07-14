7e0b91fa7aef324f9e6f9092ca4ef76c814c5563
alfonsodelr <61682073+alfonsodelr@users.noreply.github.com>
2024-07-13 22:22:49 -0700

    Initialized Next App

---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div style={styles.container}>
+      <h1>Alfonso Luis Del Rosario</h1>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    margin: 0,
+    padding: 0,
+  }
+};
